fbbbfa0e2f0e2d5e1e86abe893aa1f718b887c10
Dadivaldo <dadivaldomendonca010@gmail.com>
2026-06-21 22:35:00 +0100

    Init repo

---
+++ app/src/main/java/com/dadneedsreport/DadNeedsReportApplication.java
@@ -0,0 +1,13 @@
+package com.dadneedsreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadNeedsReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadNeedsReportApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/dadneedsreport/DadNeedsReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dadneedsreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadNeedsReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
