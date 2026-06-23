3ddccbd7730c093c3605a13b65e814d34bf3ac8f
Dadivaldo <dadivaldomendonca010@gmail.com>
2026-06-23 11:16:20 +0100

    feat: Add updated endpoint

--- app/src/main/java/com/dadneedsreport/controllers/TransactionController.java
+++ app/src/main/java/com/dadneedsreport/controllers/TransactionController.java
@@ -6,14 +6,17 @@ import com.dadneedsreport.dto.TransactionRequest;
 import com.dadneedsreport.dto.TransactionResponse;
 import com.dadneedsreport.services.TransactionService;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("api")
@@ -32,6 +35,17 @@ public class TransactionController {
 			return ResponseEntity.status(HttpStatus.CREATED).body(null);
 		} catch (RuntimeException e) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}
+	}
+
+
+	@PutMapping("transactions/{id}")
+	public ResponseEntity<String> transactionsById(@PathVariable Long id, @Valid @RequestBody TransactionRequest entity) {
+		try {
+			transactionService.saveTransaction(entity);
+			return ResponseEntity.status(HttpStatus.CREATED).body(null);
+		} catch (EntityNotFoundException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
 		}
 	}
 
--- app/src/main/java/com/dadneedsreport/services/TransactionService.java
+++ app/src/main/java/com/dadneedsreport/services/TransactionService.java
@@ -2,15 +2,17 @@ package com.dadneedsreport.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import com.dadneedsreport.dto.TransactionRequest;
 import com.dadneedsreport.dto.TransactionResponse;
-import com.dadneedsreport.models.BaseTransact;
 import com.dadneedsreport.models.Transaction;
 import com.dadneedsreport.models.TransactionHistory;
 import com.dadneedsreport.repositories.TransactionHistoryRepository;
 import com.dadneedsreport.repositories.TransactionRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class TransactionService {
@@ -22,7 +24,6 @@ public class TransactionService {
 		this.incomeEntity = incomeEntity;
 		this.auditoria = transactionHistoryRepository;
 	}
-
 
 	public List<TransactionResponse> getTransactionsAuditoria() {
 		List<TransactionHistory> transacts = auditoria.findAll();
@@ -38,12 +39,19 @@ public class TransactionService {
 		return (response);
 	}
 
-
 	public void saveTransaction(TransactionRequest entity) {
 		Transaction transaction = new Transaction();
 		transaction.prepareToPersist(entity);
 		incomeEntity.save(transaction);
 		auditoria.save(TransactionHistory.from(transaction));
+	}
+
+	public void updateTransactionById(Long id, TransactionRequest entity) {
+		Transaction transact = incomeEntity.findById(id)
+				.orElseThrow(() -> new EntityNotFoundException("User not found"));
+		transact.prepareToPersist(entity);
+		incomeEntity.save(transact);
+		auditoria.save(TransactionHistory.from(transact));
 	}
 
 	public List<TransactionResponse> getTransactions() {
