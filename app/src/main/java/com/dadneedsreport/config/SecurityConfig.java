2bb15e20dc89a5e3ac92acf68ea8549e9affa6ed
Dadivaldo Mendonca <dadmendo@c2r4s4.42luanda.com>
2026-07-07 18:22:24 +0100

    feat: Add cors Configuration

--- app/src/main/java/com/dadneedsreport/config/SecurityConfig.java
+++ app/src/main/java/com/dadneedsreport/config/SecurityConfig.java
@@ -1,5 +1,12 @@
 package com.dadneedsreport.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.util.Arrays;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
@@ -53,6 +60,7 @@ public class SecurityConfig {
 						}))
 				.formLogin(form -> form.disable())
 				.httpBasic(basic -> basic.disable())
+				.cors(withDefaults())
 				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 		return http.build();
 	}
@@ -64,5 +72,22 @@ public class SecurityConfig {
 
 	public static Pageable getFindAllLimit() {
 		return PageRequest.of(0, 60);
-	} 
+	}
+
+	@Bean
+	public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration cors = new CorsConfiguration();
+		cors.setAllowedOriginPatterns(Arrays.asList("*"));
+		cors.setAllowedHeaders(Arrays.asList("*"));
+		cors.setAllowedMethods(Arrays.asList(
+			"POST",
+			"GET",
+			"DELETE",
+			"PUT",
+			"OPTIONS"
+		));
+		UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
+		configSource.registerCorsConfiguration("/**", cors);
+		return configSource;
+	}
 }
