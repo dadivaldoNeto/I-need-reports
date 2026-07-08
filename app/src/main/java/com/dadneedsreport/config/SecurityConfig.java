285c6558883a2b13f555968d9432cd143343bb70
Dadivaldo <dadivaldomendonca010@gmail.com>
2026-07-08 12:15:54 +0100

    wip

--- app/src/main/java/com/dadneedsreport/config/SecurityConfig.java
+++ app/src/main/java/com/dadneedsreport/config/SecurityConfig.java
@@ -84,6 +84,7 @@ public class SecurityConfig {
 			"GET",
 			"DELETE",
 			"PUT",
+			"PATCH",
 			"OPTIONS"
 		));
 		UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
